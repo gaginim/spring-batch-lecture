de3d0cf55b4b7eb63dc4589bea390bc40301299e
wanchub429 <wanchub429@gmail.com>
2024-01-14 15:08:22 +0900

    Initial commit

---
+++ BookMs/src/main/java/com/msa/rental/BookMsApplication.java
@@ -0,0 +1,13 @@
+package com.msa.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMsApplication.class, args);
+    }
+
+}

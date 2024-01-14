2d2fc9ad23b4ccabaae453ddcc058db71ca50959
wanchub429 <wanchub429@gmail.com>
2024-01-14 15:42:28 +0900

    book entity 생성

--- BookMs/src/main/java/com/msa/rental/domain/model/Book.java
+++ BookMs/src/main/java/com/msa/rental/domain/model/Book.java
@@ -3,9 +3,9 @@ package com.msa.rental.domain.model;
 import com.msa.rental.domain.model.vo.BookStatus;
 import com.msa.rental.domain.model.vo.Classification;
 import com.msa.rental.domain.model.vo.Location;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.msa.rental.domain.model.vo.Source;
+import java.time.LocalDateTime;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
@@ -18,4 +18,47 @@ public class Book {
   private Classification classification;
   private BookStatus bookStatus;
   private Location location;
+
+  @Builder(access = AccessLevel.PRIVATE, toBuilder = true)
+  public Book(
+      String title,
+      BookDesc desc,
+      Classification classification,
+      BookStatus bookStatus,
+      Location location) {
+    this.title = title;
+    this.desc = desc;
+    this.classification = classification;
+    this.bookStatus = bookStatus;
+    this.location = location;
+  }
+
+  public static Book createBook(
+      String title,
+      String author,
+      String isbn,
+      String description,
+      LocalDateTime publishDt,
+      Source source,
+      Classification classification,
+      Location location) {
+
+    return Book.builder()
+        .title(title)
+        .desc(BookDesc.createBookDesc(author, isbn, description, publishDt, source))
+        .classification(classification)
+        .location(location)
+        .bookStatus(BookStatus.ENTERED)
+        .build();
+  }
+
+  public Book makeAvailable() {
+    this.bookStatus = BookStatus.AVAILABLE;
+    return this;
+  }
+
+  public Book makeUnAvailable() {
+    this.bookStatus = BookStatus.UNAVAILABLE;
+    return this;
+  }
 }
--- BookMs/src/main/java/com/msa/rental/domain/model/BookDesc.java
+++ BookMs/src/main/java/com/msa/rental/domain/model/BookDesc.java
@@ -1,14 +1,10 @@
 package com.msa.rental.domain.model;
 
 import com.msa.rental.domain.model.vo.Source;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
+import lombok.*;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class BookDesc {
 
@@ -17,4 +13,33 @@ public class BookDesc {
   private String isbn;
   private LocalDateTime publicationDate;
   private Source source;
+
+  @Builder(access = AccessLevel.PRIVATE, toBuilder = true)
+  public BookDesc(
+      String author,
+      String isbn,
+      String description,
+      LocalDateTime publicationDate,
+      Source source) {
+    this.author = author;
+    this.isbn = isbn;
+    this.description = description;
+    this.publicationDate = publicationDate;
+    this.source = source;
+  }
+
+  public static BookDesc createBookDesc(
+      String author,
+      String isbn,
+      String description,
+      LocalDateTime publicationDate,
+      Source source) {
+    return BookDesc.builder()
+        .author(author)
+        .isbn(isbn)
+        .description(description)
+        .publicationDate(publicationDate)
+        .source(source)
+        .build();
+  }
 }

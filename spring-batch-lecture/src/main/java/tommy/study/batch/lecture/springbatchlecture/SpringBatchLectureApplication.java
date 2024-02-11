66b31f9373d2da929d20f45b6b60ae094cbf7aef
wanchub429 <wanchub429@gmail.com>
2024-02-11 19:10:56 +0900

    spring batch test

---
+++ spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/SpringBatchLectureApplication.java
@@ -0,0 +1,14 @@
+package tommy.study.batch.lecture.springbatchlecture;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchLectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchLectureApplication.class, args);
+  }
+}
---
+++ spring-batch-lecture/src/test/java/tommy/study/batch/lecture/springbatchlecture/SpringBatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package tommy.study.batch.lecture.springbatchlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

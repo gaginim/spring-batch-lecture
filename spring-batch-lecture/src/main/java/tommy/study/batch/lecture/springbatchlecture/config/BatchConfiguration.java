f41158e4fad016eb9ead32d509a1c6fce9688d99
wanchub429 <wanchub429@gmail.com>
2024-02-11 21:16:44 +0900

    spring batch basic

--- spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/config/BatchConfiguration.java
+++ spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/config/BatchConfiguration.java
@@ -3,4 +3,4 @@ package tommy.study.batch.lecture.springbatchlecture.config;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
 @EnableBatchProcessing
-public class TommyBatchConfiguration {}
+public class BatchConfiguration {}
--- spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/job/HelloJobConfiguration.java
+++ spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/job/HelloJobConfiguration.java
@@ -2,8 +2,10 @@ package tommy.study.batch.lecture.springbatchlecture.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -15,8 +17,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class HelloJobConfiguration {
 
   @Bean
-  public Job myJob(JobRepository jobRepository, Step myStep) {
-    return new JobBuilder("myJob", jobRepository).start(myStep).build();
+  public Job myJob(JobRepository jobRepository, Step myStep, Step myNextStep) {
+    return new JobBuilder("myJob", jobRepository).start(myStep).next(myNextStep).build();
   }
 
   @Bean
@@ -28,9 +30,31 @@ public class HelloJobConfiguration {
   }
 
   @Bean
+  public Step myNextStep(
+      JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+
+    return new StepBuilder("myNextStep", jobRepository)
+        .tasklet(
+            new Tasklet() {
+              @Override
+              public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+                  throws Exception {
+                System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+                System.out.println(" >>> next work tommy");
+                System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+                return RepeatStatus.FINISHED;
+              }
+            },
+            transactionManager)
+        .build();
+  }
+
+  @Bean
   public Tasklet myTasklet() {
     return (contribution, chunkContext) -> {
+      System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
       System.out.println(" >>> hello tommy ");
+      System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
       return RepeatStatus.FINISHED;
     };
   }

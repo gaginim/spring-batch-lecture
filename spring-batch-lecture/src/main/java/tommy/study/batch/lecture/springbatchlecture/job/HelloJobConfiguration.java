9977b30db133bc169469371c0f44d0e7098df5f9
tommy <tommy.han@kakaoenterprise.com>
2024-02-19 11:28:02 +0900

    job name 변경

--- spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/job/HelloJobConfiguration.java
+++ spring-batch-lecture/src/main/java/tommy/study/batch/lecture/springbatchlecture/job/HelloJobConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -16,24 +17,34 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class HelloJobConfiguration {
 
-  @Bean
-  public Job myJob(JobRepository jobRepository, Step myStep, Step myNextStep) {
+  private final String JOB_NAME = "myJob";
+  private final String STEP1_NAME = JOB_NAME + "_step1";
+  private final String STEP2_NAME = JOB_NAME + "_step2";
+  private final String TASK1_NAME = STEP1_NAME + "_task1";
+
+  @Bean(JOB_NAME)
+  public Job myJob(
+      JobRepository jobRepository,
+      @Qualifier(STEP1_NAME) Step myStep,
+      @Qualifier(STEP2_NAME) Step myNextStep) {
     return new JobBuilder("myJob", jobRepository).start(myStep).next(myNextStep).build();
   }
 
-  @Bean
+  @Bean(STEP1_NAME)
   public Step myStep(
       JobRepository jobRepository,
-      Tasklet myTasklet,
+      @Qualifier(TASK1_NAME) Tasklet myTasklet,
       PlatformTransactionManager transactionManager) {
-    return new StepBuilder("myStep", jobRepository).tasklet(myTasklet, transactionManager).build();
+    return new StepBuilder(STEP1_NAME, jobRepository)
+        .tasklet(myTasklet, transactionManager)
+        .build();
   }
 
-  @Bean
+  @Bean(STEP2_NAME)
   public Step myNextStep(
       JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 
-    return new StepBuilder("myNextStep", jobRepository)
+    return new StepBuilder(STEP2_NAME, jobRepository)
         .tasklet(
             new Tasklet() {
               @Override
@@ -49,7 +60,7 @@ public class HelloJobConfiguration {
         .build();
   }
 
-  @Bean
+  @Bean(TASK1_NAME)
   public Tasklet myTasklet() {
     return (contribution, chunkContext) -> {
       System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
